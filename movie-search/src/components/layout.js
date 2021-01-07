834492ba10595bb891f7f7d7df4518451a7db096
anil <anilyildirim3pc@gmail.com>
2021-01-07 21:41:35 +0100

    feat(project-setup): improve initial setup and add sass

---
+++ movie-search/src/components/layout.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const ListLink = props => (
+  <li>
+    <Link to={ props.to }>{ props.children }</Link>
+  </li> 
+)
+
+export default function Layout({ children }) {
+  return (
+    <div className="page-wrapper">
+      <header>
+        <div className="navigation">
+          <Link to="/">
+            <span className="main-logo-text">MovieFinder</span>
+          </Link>
+          <nav>
+            <ul>
+              <ListLink to="/">Home</ListLink>
+              <ListLink to="/404/">Not Found</ListLink>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <div className="page-content">
+        {children}
+      </div>
+      <footer className="footer">
+        <nav aria-label="footer navigation">
+          <ul className="footer__inner">
+            <ListLink to="/">Home</ListLink>
+            <ListLink to="/404/">Not Found</ListLink>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  )
+}
